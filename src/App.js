085547ee8e2c261972a77e886b20862831c403a9
Vadim Kononov <vadim@poetic.com>
2019-10-26 09:13:15 -0500

    Add a button and a click counter.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {PureComponent} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+    state = {
+        clickCount: 0
+    };
+
+    handleClickEvent = () => {
+      this.setState({clickCount: this.state.clickCount + 1});
+    };
+
+    render() {
+        return (
+            <div>
+                <p>You have clicked this button {this.state.clickCount} times.</p>
+                <button onClick={this.handleClickEvent}>Button</button>
+            </div>
+        );
+    }
 }
 
 export default App;
